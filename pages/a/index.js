bc44cd087107eda074460ab4b158096f8f06030e
Rahul Raghavendra Choudhury <87576737+rahrag@users.noreply.github.com>
2022-04-04 21:06:58 +0530

    adsfaS

--- pages/a/index.js
+++ pages/a/index.js
@@ -7,7 +7,7 @@ import Link from 'next/link'
 const About = () => {
   return (
     <div>
-        <div classname = ""></div>
+        <div className = ""></div>
         <div className="sm:p-20 text-center sm:text-center">
            <motion.p className="text-xl sm:text-3xl text-white"> Lorem Ipsum is simply dummy . What we do is somethin beyond your mind. You must enroll for the events. There&apos;s no other option! Okay?</motion.p>
            </div>
