f37fff8702c846fe74ca3631a3c2671e119da37c
Rahul Raghavendra Choudhury <87576737+rahrag@users.noreply.github.com>
2022-04-04 20:55:50 +0530

    about us page part 1

--- pages/a/index.js
+++ pages/a/index.js
@@ -1,15 +1,16 @@
 import React from 'react'
+import styles from '../../styles/Home.module.css'
+import { motion } from 'framer-motion'
+import Link from 'next/link'
+
 
 const About = () => {
   return (
     <div>
-        <p className='text-8xl'>about</p>
-        <p className='text-8xl'>about</p>
-        <p className='text-8xl'>about</p>
-        <p className='text-8xl'>about</p>
-        <p className='text-8xl'>about</p>
-        <p className='text-8xl'>about</p>
-        <p className='text-8xl'>about</p>
+        <div className="sm:p-20 text-center sm:text-center">
+           <motion.p className="text-xl sm:text-3xl text-white"> Lorem Ipsum is simply dummy . What we do is somethin beyond your mind. You must enroll for the events. There's no other option! Okay?</motion.p>
+           </div>
+        
     </div>
   )
 }
