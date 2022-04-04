4569328b74d45c2b8d5c4e90cc45ab3450e803ed
Rahul Raghavendra Choudhury <87576737+rahrag@users.noreply.github.com>
2022-04-04 20:59:59 +0530

    about us page 2

--- pages/a/index.js
+++ pages/a/index.js
@@ -7,6 +7,7 @@ import Link from 'next/link'
 const About = () => {
   return (
     <div>
+        <div classname = ""></>
         <div className="sm:p-20 text-center sm:text-center">
            <motion.p className="text-xl sm:text-3xl text-white"> Lorem Ipsum is simply dummy . What we do is somethin beyond your mind. You must enroll for the events. There's no other option! Okay?</motion.p>
            </div>
