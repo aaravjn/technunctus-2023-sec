77d35ae55a1a377f5d6c32cb93baf499c1e41d36
Spider8019 <spider8019official@gmail.com>
2022-04-24 22:11:27 +0530

    fadfad

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,18 @@ import '../styles/globals.css'
 import Layout from "../layout/Basic"
 import {AnimatePresence,motion} from "framer-motion"
 import {pageAnimation} from "../utils/framer"
+import Head from "next/head"
 
 function MyApp({ Component, pageProps,router }) {
   return(
     <Layout>
+      <Head>
+        <link rel="shortcut icon" href="/static/logo.png" />
+        <meta charset="UTF-8"/>
+        <meta name="description" content="The IIT Jammu family takes immense pleasure in inviting you all to the third edition of our Annual Tech Fest, Technunctus’21, which is scheduled from 17th-18th April 2021 in complete Online Mode"/>
+        <meta name="keywords" content="TECHFEST, spider8019, IIT JAMMU, TECHNUNCTUS"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>  
+      </Head>
       <AnimatePresence exitBeforeEnter>
 
          <motion.div
---
+++ pages/_document.js
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+    return (
+        <Html>
+            <Head>
+                <link rel="shortcut icon" href="/static/logo.png?v=2" />
+                <meta charset="UTF-8" />
+                <meta name="description" content="The IIT Jammu family takes immense pleasure in inviting you all to the third edition of our Annual Tech Fest, Technunctus’21, which is scheduled from 17th-18th April 2021 in complete Online Mode" />
+                <meta name="keywords" content="TECHFEST, spider8019, IIT JAMMU, TECHNUNCTUS" />
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    )
+}
